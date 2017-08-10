dba2e499ad0519784abce52b18073f4b2e3d0c50
thunderbird, inc <26640342+thunderbird-inc@users.noreply.github.com>
2017-08-10 15:19:09 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -10,8 +10,8 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'yokohama_budget';
-OpenSpending.year = '2012';
+OpenSpending.identifier = 'takasaki_budget';
+OpenSpending.year = '2017';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
 
